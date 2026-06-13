45e6dc35067f8c24b4076f70b6a43cb200daf072
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2026-06-13 15:19:11 +0530

    Time: 3 ms (50.31%), Space: 32.1 MB (6.27%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,16 +1,62 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        int ns = nums.size() ;
+        vector<int> a;
+        vector<int> b;
+        vector<int> res(ns);
 
-        int n = nums.size();
-
-        for (int i=0; i<n; i++){
-            nums[i] = nums[i]*nums[i];
+        for(int i=0; i < ns; i++){
+            if(nums[i] < 0){
+                a.push_back(nums[i]*nums[i]);
+            }
+        }
+        reverse(a.begin() , a.end());
+        for (int j=0; j<ns; j++){
+            if(nums[j] >= 0){
+                b.push_back(nums[j]*nums[j]);
+            }
         }
 
-        sort(nums.begin() , nums.end());
+        int k = a.size();
+        int l = b.size();
+        int id = 0;
 
-        return nums ; 
-        
+        int m=0; int n=0;
+
+        while (k>m && l>n){
+            if(a[m] < b[n]){
+                res[id] = a[m];
+                id++;
+                m++;
+            }
+            else if(a[m] > b[n]){
+                res[id] = b[n];
+                id++;
+                n++;
+            }
+            else{
+                res[id] = a[m] ;
+                id++ ; m++;
+                res[id] = b[n];
+                id++; n++;
+            }
+
+        }
+
+        while(k>m){
+            res[id] = a[m];
+            id++;
+            m++;
+        }
+        while(l>n){
+            res[id] = b[n];
+            id++;
+            n++;
+        }
+
+        return res;
+
+
     }
 };
