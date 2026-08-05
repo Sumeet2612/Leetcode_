5fe814c28ff41d2e30dd4181b4c726b769f7bbba
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2026-08-05 15:53:56 +0530

    Time: 21 ms (6.14%), Space: 28.1 MB (5.26%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int mis = 0 ; 
+        unordered_map<int , int> freq ;
+
+        for (int i=0; i<nums.size(); i++){
+            freq[nums[i]] ++ ;
+        }
+
+        for (int j=0; j<=freq.size(); j++){
+            if(freq.find(j) == freq.end()){
+                mis = j;
+            }
+            else {
+                continue ;
+            }
+
+        }
+
+        return mis ;
+        
+    }
+};
