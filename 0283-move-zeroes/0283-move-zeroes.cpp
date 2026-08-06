d14a404717c1272ca56ee913f677aabcc58a116b
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2026-08-06 14:45:26 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
 
+        int count = 0;
         int n = nums.size();
-        vector<int> res(n);
-        int j = 0;
 
         for (int i=0; i<n; i++){
+
             if (nums[i] != 0){
-                res[j] = nums[i];
-                j++ ;
+                swap(nums[i] , nums[count]);
+                count ++ ;
             }
-        }
-        while (j<=n){
-            res.push_back(0);
-            j++ ;
-        }
-        for (int i=0; i<n; i++){
-            nums[i] = res[i];
         }
         
     }
