ab47ac59ee16c91faddb0466e2eeef6cee211a8a
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2026-08-12 15:11:27 +0530

    Time: 0 ms (100%), Space: 11.8 MB (50.24%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        unordered_map<char , int> freq;
+
+        for (int i=0; i<magazine.length(); i++){
+
+            freq[magazine[i]]++;
+        }
+
+        for (int i=0; i<ransomNote.length(); i++){
+            if (freq.find(ransomNote[i]) != freq.end() && freq[ransomNote[i]] > 0){
+                freq[ransomNote[i]]--;
+            }
+            else {
+                return false ;
+            }
+        }
+        return true ;
+    }
+};
